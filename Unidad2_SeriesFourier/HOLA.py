680cde9b09fd9fbef4f95591f45b4f20dd943d92
Isabel7w7 <mariaisabelperezgarcia0991@gmail>
2025-10-13 15:12:45 -0600

    Repositorio de analisis y senales

---
+++ Unidad2_SeriesFourier/HOLA.py
@@ -0,0 +1,9 @@
+print("HOLA MUNDO")
+#usar python como calculadora
+print("2+2 = "+str(2+2))
+print("8*6 = "+str(8*6))
+print("50-5 = "+str(50-5))
+print("7^2 = "+str(7**2))
+print("100/5 ="+str(100/5))
+print("100//6 ="+str(100//6))
+print("500%60 = "+str(500%60))
